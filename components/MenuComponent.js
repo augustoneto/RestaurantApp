cf579adc1b57f489a1328c39a708293df1d0570d
Antonio Augusto Silva Oliveira Neto <augusto.soliveiran@gmail.com>
2020-03-11 01:04:38 -0300

    App react-native first commit

---
+++ components/MenuComponent.js
@@ -0,0 +1,50 @@
+import React, { Component } from 'react';
+import { View, FlatList } from 'react-native';
+import { Tile } from 'react-native-elements';
+import { DISHES } from '../shared/dishes';
+import { connect } from 'react-redux';
+import { baseUrl } from '../shared/baseUrl';
+
+const mapStateToProps = state => {
+    return {
+      dishes: state.dishes
+    }
+  }
+
+class Menu extends Component {
+
+    static navigationOptions = {
+        title: 'Menu'
+    };
+
+    render() {
+        const renderMenuItem = ({item, index}) => {
+
+            return (
+                    <Tile
+                        key={index}
+                        title={item.name}
+                        caption={item.description}
+                        featured
+                        onPress={() => navigate('Dishdetail', { dishId: item.id })}
+                        imageSrc={{ uri: baseUrl + item.image}}
+                      />
+            );
+        };
+
+        const { navigate } = this.props.navigation;
+
+        return (
+            <FlatList 
+                data={this.props.dishes.dishes}
+                renderItem={renderMenuItem}
+                keyExtractor={item => item.id.toString()}
+                />
+        );
+    }
+
+    
+}
+
+
+export default connect(mapStateToProps)(Menu);
