e234e3df4b1e024a06942c65bcf6323c9b98b62b
Antonio Augusto Silva Oliveira Neto <augusto.soliveiran@gmail.com>
2020-08-12 21:03:46 -0300

    Animatable

--- components/MenuComponent.js
+++ components/MenuComponent.js
@@ -5,6 +5,7 @@ import { Tile } from 'react-native-elements';
 import { DISHES } from '../shared/dishes';
 import { connect } from 'react-redux';
 import { baseUrl } from '../shared/baseUrl';
+import * as Animatable from 'react-native-animatable';
 
 const mapStateToProps = state => {
     return {
@@ -22,6 +23,7 @@ class Menu extends Component {
         const renderMenuItem = ({item, index}) => {
 
             return (
+                <Animatable.View animation="fadeInRightBig" duration={2000}>
                     <Tile
                         key={index}
                         title={item.name}
@@ -30,6 +32,7 @@ class Menu extends Component {
                         onPress={() => navigate('Dishdetail', { dishId: item.id })}
                         imageSrc={{ uri: baseUrl + item.image}}
                       />
+                </Animatable.View>
             );
         };
 
